85aa9ffd29436325b456fe5939fe2a49654891f1
Mykola <mykola22dubyna@gmail.com>
2024-03-12 17:34:42 +0200

    Remove redundant comment

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-# "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "kitchen-servise.onrender.com"]
 
