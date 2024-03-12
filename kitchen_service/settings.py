152889f751691673407a58a01bb5c82a15cf5d91
Mykola <mykola22dubyna@gmail.com>
2024-03-12 15:51:14 +0200

    Remove django secret key and change DEBUG

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY","django-insecure-x&ov(111#pi*$3verbp_pwaprj^*$j+cqb^0g8b@52l9&fvq04")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
