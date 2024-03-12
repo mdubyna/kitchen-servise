a18ccf6bf91f417181d3c62464fa471d62a63be8
Mykola <mykola22dubyna@gmail.com>
2024-03-12 16:14:00 +0200

    Add kitchen-servise.onrender.com to ALLOWED_HOSTS

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "RENDER" not in os.environ
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "kitchen-servise.onrender.com"]
 
 
 # Application definition
