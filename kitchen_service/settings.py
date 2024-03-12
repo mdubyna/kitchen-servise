f328e43c1a37438193b2d6f94cc3e2fa3acd0884
Mykola <mykola22dubyna@gmail.com>
2024-03-12 16:21:58 +0200

    Update DEBUG for search problems

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -26,7 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+
+# "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "kitchen-servise.onrender.com"]
 
